a86b5480c8285200faaab10ca2f06522cd58e2be
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2023-06-11 01:12:09 +0200

    feat: add play and pause buttons, change app features description, add test gif,

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ let h1 = document.querySelector('#h1');
 let h2 = document.querySelector('#h2');
 let credit = document.querySelector('#credit');
 let guesses=10;
-let play=document.querySelector('#playAgain');
+let pause=document.querySelector('#pause');
+let resume = document.querySelector('#resume');
 let audio = document.querySelector('#audio');
 let audio2 = document.querySelector('#audio2');
 let audio3 = document.querySelector('#audio3');
@@ -12,9 +13,10 @@ let p = document.getElementById('p');
 let wins = 0;
 let img = document.getElementById('img');
 let img2 = document.getElementById('img2');
-
+let img3 = document.getElementById('img3');
+let randomNumber = Math.floor(Math.random() * 10);
 ;
-p.innerText = "Welcome "  + user + ` Guess the number from 0 to 9.
+p.innerText = "Welcome "  + user + `! Guess the number from 0 to 9.
 The number changes with every try. 
 Press 'guess the number' button to start the game. 
 Press 'Pause the game' button to pause.
@@ -50,10 +52,29 @@ greeting();
     
 // }
 
+// function stop(){
+    
+    
+
+
+
+
+// }  
+
+
+// function resume(){
+
+
+// h1.innerHTML = "";
+// checkNumber();
+
+
+// }
+
 function checkNumber(){
     audio.play();
     
-    let randomNumber = Math.floor(Math.random() * 10);
+    
     
     let param = parseInt(prompt(user + " ,enter a number to guess it between 0 and 9 "));
     // if (playAgain){
@@ -65,17 +86,27 @@ function checkNumber(){
         img.style.visibility = "visible";
         img.style.display = "block";
         img2.style.display = "none";
+        img3.style.display = "none";
         audio.pause();
         audio3.play();
         p.innerText = "Congratulations, " + user + " ! The right number is " + randomNumber + ". You won " + (wins=wins+1) + " time(s)! Try more!";
         h1.innerText = "";
-        h2.innerText = "";
+        // h2.innerText = "";
+        randomNumber = Math.floor(Math.random() * 10);
+        // (function(){
+        //     if(img.style.display==="block")
+        //     setTimeout(2000, (img.style.display=="none"));
+        // })()
         
+        // if(wins===2){
+        //     let imgSrc = img.getAttribute;
+        //     imgSrc.setAttribute = "giphy.gif";
+        // }
 
     }
     else if(randomNumber>param){
     
-        h1.innerText = user + " ,the right number was " + randomNumber + " .Continue trying!";
+        h1.innerText = user + " ,the right number was bigger .Continue trying!";
         // h2.innerText = randomNumber;
         p.innerText = "";
       showGuesses();  
@@ -85,7 +116,7 @@ function checkNumber(){
 else if(randomNumber<param){
 
 
-    h1.innerText = user + " ,the right number was " + randomNumber + " .Continue trying!";
+    h1.innerText = user + " ,the right number was smaller .Continue trying!";
     p.innerText = "";
     // h2.innerText = randomNumber;
     showGuesses();
@@ -100,6 +131,9 @@ else if (isNaN(param)){
          
 }
 }
+
+
+
 
 function showGuesses(){
     guesses=guesses-1;
@@ -122,19 +156,22 @@ function gameOver(){
     }
 }
 
-function playAgain(){
-    h1.innerText = user + " ,you paused the game! Press 'guess the number' button to resume";
-    audio.pause();
-  
-}
+
 
 function success(){
     audio3.play();
+    randomNumber = Math.floor(Math.random() * 10);
 }
 
-play.addEventListener("click", function(){
-    playAgain();
+pause.addEventListener("click", function(){
+    audio.pause();
+    audio3.pause();
+    h1.innerHTML = user + " ,you paused the game! Press again to resume";
 });
+
+// resume.addEventListener("click", function(){
+//     resume();
+// });
 
  
 
